40a21f8ed00016fe31d5fa5fd8b026be919f95c8
Dagmara <dagmarakitkowska@gmail.com>
2020-01-12 13:36:36 +0100

    drugi

--- src/Zad10_PoczatekObiektowosci.java
+++ src/Zad10_PoczatekObiektowosci.java
@@ -1,11 +1,77 @@
 /**
- * ...comment class...
+ * Poczatki obiektowosci z Piotrem
  *
  * @author dagmarakitkowska@gmail.com
  * @since 12.01.2020
  */
+class Samochod {
+    private String kolor;
+    private int moc;
+    private static int iloscKol;
+
+    static {
+        iloscKol = 4;
+    }
+
+    Samochod(String kolor, int moc) {
+        setKolor(kolor);
+        setMoc(moc);
+    }
+
+    public Samochod(int moc){
+        this("Brak", moc);
+    }
+
+    public Samochod(String kolor){
+        this(kolor, 0);
+    }
+
+    private void setMoc(int moc) {
+        if (moc > 0) {
+            this.moc = moc;
+        }
+    }
+
+
+    public void setKolor(String kolor) {
+        this.kolor = kolor;
+    }
+
+    public double getCzasPrzebycia1000(){
+        return 1000/(double)this.moc;
+    }
+
+    public String toString() {
+        return "[kolor: " + this.kolor + " ilosc kol: " + iloscKol + " moc: " + moc + "]";
+    }
+}
+
 final class Zad10_PoczatekObiektowosci {
     public static void main(String[] args) {
+        System.out.println("Witaj");
 
+        Samochod Opel = new Samochod("Czerwony", 250);
+        Samochod Ford = new Samochod("Niebieski", -180);
+
+        System.out.println(Opel.getCzasPrzebycia1000());
+
+        System.out.println(Opel);
+        System.out.println(Ford);
+
+    }
+}
+
+class SamochorOsobowy extends Samochod{
+
+    SamochorOsobowy(String kolor, int moc) {
+        super(kolor, moc);
+
+    }
+}
+
+class SamochodCiezarowy extends Samochod{
+
+    SamochodCiezarowy(String kolor, int moc) {
+        super(kolor, moc);
     }
 }
--- src/Zad11_ZadanieObiektowe.java
+++ src/Zad11_ZadanieObiektowe.java
@@ -1,10 +1,65 @@
-package PACKAGE_NAME;
-
 /**
  * ...comment class...
  *
  * @author dagmarakitkowska@gmail.com
  * @since 12.01.2020
  */
-final class Zad11_ZadanieObiektowe {
+class Zwierze {
+
+    private String gatunek;
+    private int wiek;
+    private double waga;
+    private boolean isMale;
+
+    Zwierze(String gatunek, int wiek, double waga, boolean isMale) {
+        setGatunek(gatunek);
+        setWiek(wiek);
+        setWaga(waga);
+        setMale(isMale);
+    }
+
+    private void setGatunek(String gatunek) {
+        this.gatunek = gatunek;
+    }
+
+    private void setWiek(int wiek) {
+        if (wiek > 0) {
+            this.wiek = wiek;
+        }
+    }
+
+    private void setWaga(double waga) {
+        if (waga > 0) {
+            this.waga = waga;
+        }
+    }
+
+    private void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public double getDziennaPorcjaKarmienia(){
+        return this.waga/10;
+    }
+
+    @Override
+    public String toString() {
+        return "ZWIERZE:" +
+                "gatunek: " + gatunek +
+                ", wiek: " + wiek +
+                ", waga: " + waga +
+                ", isMale: " + isMale;
+    }
 }
+
+final class Zad11_ZadanieObiektowe {
+    public static void main(String[] args) {
+
+        Zwierze lew = new Zwierze("Lew", 8, 19.5, true);
+        System.out.println(lew);
+        System.out.println(lew.getDziennaPorcjaKarmienia());
+
+    }
+}
+
+
