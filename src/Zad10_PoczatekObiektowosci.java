b01fd435de1f1ea9f466cfd384b8c99d589ed879
Dagmara <dagmarakitkowska@gmail.com>
2020-01-12 13:31:06 +0100

    zajecia piotra

---
+++ src/Zad10_PoczatekObiektowosci.java
@@ -0,0 +1,11 @@
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 12.01.2020
+ */
+final class Zad10_PoczatekObiektowosci {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Zad11_ZadanieObiektowe.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 12.01.2020
+ */
+final class Zad11_ZadanieObiektowe {
+}
